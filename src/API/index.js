a8003878d89bf4589f2a793032ea3ff41634924c
denisshttps://github.com/Deniss28 <deniss97138@gmail.com>
2022-07-24 18:06:49 -0500

    fix bugs

--- src/API/index.js
+++ src/API/index.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
+const URL = import.meta.VITE_URL;
 export const SendEmail = async ({ name, email, message, setSend }) => {
   try {
     const data = { name, email, message, setSend };
-    let res = await axios.post(`http://localhost:5000/send`, data);
+    let res = await axios.post(`${URL}/send`, data);
 
     if (res) {
-      setSend(res.data); //si existe la respuetsa traeme la data
+      setSend(res.data); //si existe la respuesta traeme la data
     }
   } catch (error) {
     alert(error.response.data.message);
